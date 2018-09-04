5559307a7e2c34eb0cde231a1e082eb4d364368d
Robert Snyder <r.snyder49@gmail.com>
2018-09-04 23:44:20 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods, Findable
-  include Paramable, Memorable::InstanceMethods
+  include Memorable::InstanceMethods, Paramable
   
   attr_accessor :name
   attr_reader :songs
