95668d9013dd49cc5530c73664e41f435a2f3f02
Robert Snyder <r.snyder49@gmail.com>
2018-09-04 23:44:48 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Song
   extend Memorable::ClassMethods, Findable 
-  include Paramable, Memorable::InstanceMethods  
+  include Memorable::InstanceMethods, Paramable  
   
   attr_accessor :name
   attr_reader :artist
